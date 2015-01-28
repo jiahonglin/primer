b82703e8c992a556b9cb5d4d5a6a7baf8c01043e
jiahong <jiahonglin17@gmail.com>
2015-01-28 23:04:18 +0800

    practice4_36

---
+++ p4_36.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+typedef int int_array[4];
+int main(){
+    int ia[3][4] = {
+        {0,1,2,3},
+        {4,5,6,7},
+        {8,9,10,11},
+    };
+    /*
+    for(int_array *p = ia;p != ia+3; ++p)
+        for(int *q = *p ; q != *p+4 ; ++q)
+            cout << *q << endl;
+    */
+    
+    for(int (*p)[4] = ia ; p != ia+3 ; ++p)
+        for(int *q = *p ; q != *p+4; q++ )
+             cout << q << ":" << *q << endl;
+}
+
